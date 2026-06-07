6ae5e0a96514fee26e03a8f115a2043a539bccea
Yashswani Soni <yashswani@Yashswanis-MacBook-Pro.local>
2026-06-08 00:48:09 +0530

    Initial commit for family travel tracker

---
+++ index.js
@@ -0,0 +1,107 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import pg from 'pg';
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+let currentUserId = 1;
+
+async function getUsers() {
+    const result = await db.query('SELECT * FROM users ORDER BY id ASC');
+    return result.rows;
+}
+async function checkVisited() {
+    const result = await db.query(
+        'SELECT country_code FROM visited_countries where user_id = $1',
+        [currentUserId],
+    );
+    let countries = [];
+    result.rows.forEach((country) => {
+        countries.push(country.country_code);
+    });
+    return countries;
+}
+app.get('/', async (req, res) => {
+    const users = await getUsers();
+    if (users.length === 0) {
+        return res.render('new.ejs');
+    }
+    const currentUser = users.find((user) => user.id === currentUserId);
+    const countries = await checkVisited();
+    res.render('index.ejs', {
+        countries: countries,
+        total: countries.length,
+        users: users,
+        color: currentUser.color,
+    });
+});
+app.post('/add', async (req, res) => {
+    const input = req.body['country'];
+    try {
+        const result = await db.query(
+            'SELECT country_code FROM countries WHERE LOWER(country_name) = $1',
+            [input.toLowerCase()],
+        );
+        const data = result.rows[0];
+        const countryCode = data.country_code;
+        const alreadyVisited = await db.query(
+            'SELECT * FROM visited_countries WHERE country_code = $1 AND user_id = $2',
+            [countryCode, currentUserId],
+        );
+
+        if (alreadyVisited.rows.length > 0) {
+            return res.redirect('/');
+        }
+        try {
+            await db.query(
+                'INSERT INTO visited_countries (country_code,user_id) VALUES ($1,$2)',
+                [countryCode, currentUserId],
+            );
+            res.redirect('/');
+        } catch (err) {
+            console.log(err);
+        }
+    } catch (err) {
+        console.log(err);
+    }
+});
+app.post('/user', async (req, res) => {
+    const person_id = req.body.user;
+    if (req.body.add === 'new') {
+        res.render('new.ejs');
+    } else {
+        currentUserId = Number(req.body.user);
+        res.redirect('/');
+    }
+});
+
+app.post('/new', async (req, res) => {
+    const name = req.body.name;
+    const color_name = req.body.color;
+    const result = await db.query(
+        'INSERT INTO users(name,color) VALUES ($1,$2) RETURNING id',
+        [name, color_name],
+    );
+    currentUserId = result.rows[0].id;
+    res.redirect('/');
+    //Hint: The RETURNING keyword can return the data that was inserted.
+    //https://www.postgresql.org/docs/current/dml-returning.html
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
