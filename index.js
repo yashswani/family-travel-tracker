3c8a9e068882b18e41e9e1c9d40bb6e26984abac
Yashswani Soni <yashswani@Yashswanis-MacBook-Pro.local>
2026-06-08 01:50:27 +0530

    Use DATABASE_URL for Render Postgres

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 db.connect();
 
