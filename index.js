5075bad880481ae7b3e3d22c1840c6e370f5b08d
Yashswani Soni <yashswani@Yashswanis-MacBook-Pro.local>
2026-06-08 01:09:08 +0530

    Prepare app for Render deployment

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import pg from 'pg';
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const db = new pg.Client({
   user: process.env.PG_USER,
